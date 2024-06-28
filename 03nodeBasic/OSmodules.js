97cd0b8b9dcb6160607d5b87947b4e95201a407d
Ashish <127130546+akrathor18@users.noreply.github.com>
2024-06-28 19:41:44 +0530

    commit

---
+++ 03nodeBasic/OSmodules.js
@@ -0,0 +1,9 @@
+const os = require('node:os')
+console.log(os.freemem());
+console.log(os.arch());
+console.log(os.cpus());
+console.log(os.hostname());
+console.log(os.totalmem());
+console.log(os.uptime());
+console.log(os.type());
+console.log(os.networkInterfaces());
