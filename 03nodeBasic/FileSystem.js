9240964b066f83f405458bad0d211c8fbf82774d
Ashish <127130546+akrathor18@users.noreply.github.com>
2024-06-29 19:51:30 +0530

    commit

--- 03nodeBasic/FileSystem.js
+++ 03nodeBasic/FileSystem.js
@@ -1,6 +1,6 @@
 // const { log } = require('node:console');
 const fs= require('node:fs')
-
+const sum = require('./module.mjs')
  const a=fs.readFileSync('text.txt', 'utf-8',(err, data)=>{
     console.log(err, data);
 })
@@ -11,3 +11,6 @@ fs.writeFileSync('text.txt','This is new content writen by write File function',
 })
 console.log(a);
 console.log('Reading code finish'); 
+
+
+// sum(1,1)
