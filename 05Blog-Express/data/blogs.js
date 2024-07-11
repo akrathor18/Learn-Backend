71c7a8b1ab5d4147e604f3d92ae20030b01de727
Ashish <127130546+akrathor18@users.noreply.github.com>
2024-07-11 13:12:58 +0530

    commit

---
+++ 05Blog-Express/data/blogs.js
@@ -0,0 +1,24 @@
+const bolgs=[
+    {
+    title :'How to get started with javascript',
+    content:'Learn javascript in 30daya'
+    },
+    {
+    title :'How to get started with Java',
+    content:'Learn Java in 30daya'
+    },
+    {
+    title :'How to get started with python',
+    content:'Learn python in 30daya'
+    },
+    {
+    title :'How to get started with Nodejs',
+    content:'Learn Nodejs in 30daya'
+    },
+    {
+    title :'How to get started with MySQL',
+    content:'Learn MySQL in 30daya'
+    },
+]
+
+module.exports=bolgs;
--- 05Blog-Express/routes/blog.js
+++ 05Blog-Express/routes/blog.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const routes = express.Router()
 const path= require('path')
+const bolgs= require('../data/blogs')
 
 routes.get('/', (req, res) => {
-    res.send('hii')
+    res.send(bolgs)
+    
     res.sendFile(path.join(__dirname,'../templates/index.html'))
 })
 
