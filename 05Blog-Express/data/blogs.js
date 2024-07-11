d0d6244e3d4e5466616fefba1449bede44aa6f94
Ashish <127130546+akrathor18@users.noreply.github.com>
2024-07-11 14:04:31 +0530

    commit

--- 05Blog-Express/data/blogs.js
+++ 05Blog-Express/data/blogs.js
@@ -1,23 +1,28 @@
 const bolgs=[
     {
     title :'How to get started with javascript',
-    content:'Learn javascript in 30daya'
+    content:'Learn javascript in 30daya',
+    slug:'js',
     },
     {
     title :'How to get started with Java',
-    content:'Learn Java in 30daya'
+    content:'Learn Java in 30daya',
+    slug:'java',
     },
     {
     title :'How to get started with python',
-    content:'Learn python in 30daya'
+    content:'Learn python in 30daya',
+    slug:'py',
     },
     {
     title :'How to get started with Nodejs',
-    content:'Learn Nodejs in 30daya'
+    content:'Learn Nodejs in 30daya',
+    slug:'node',
     },
     {
     title :'How to get started with MySQL',
-    content:'Learn MySQL in 30daya'
+    content:'Learn MySQL in 30daya',
+    slug:'MySQL',
     },
 ]
 
--- 05Blog-Express/routes/blog.js
+++ 05Blog-Express/routes/blog.js
@@ -1,12 +1,26 @@
 const express = require('express')
 const routes = express.Router()
 const path= require('path')
-const bolgs= require('../data/blogs')
+const blogs= require('../data/blogs')
 
+routes.get('/blogs', (req, res) => {
+    // res.send(bolgs)
+    blogs.forEach(e=> {
+        console.log(e.title);
+    });
+    res.sendFile(path.join(__dirname,'../templates/index.html'))
+})
 routes.get('/', (req, res) => {
-    res.send(bolgs)
+    res.send('Hiii')
     
     res.sendFile(path.join(__dirname,'../templates/index.html'))
+})
+routes.get('/blogpost/:slug', (req, res) => {
+    myblog= blogs.filter((e)=>{
+      return  e.slug== req.params.slug
+    })
+    console.log(myblog);
+    res.sendFile(path.join(__dirname,'../templates/blogpage.html'))
 })
 
 module.exports = routes
