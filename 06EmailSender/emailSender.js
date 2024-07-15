8b4c70b7f9a07b1382e52cd7212698e937fa5f58
Ashish <127130546+akrathor18@users.noreply.github.com>
2024-07-15 20:33:38 +0530

    commit

---
+++ 06EmailSender/emailSender.js
@@ -0,0 +1,24 @@
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: '',
+        pass: ''
+    }
+});
+
+var mailOptions = {
+    from: 'ashishkunar678@gmail.com',
+    to: 'akrathor18710@gmail.com',
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Email sent: ' + info.response);
+    }
+});
