06dfdab45c6ffd9508184595f13ed59d0a9b7da3
Ashish <127130546+akrathor18@users.noreply.github.com>
2024-07-16 20:15:08 +0530

    commit

--- 07MySQL-database/MySQL-connection.js
+++ 07MySQL-database/MySQL-connection.js
@@ -1,19 +1,34 @@
-var mysql = require('mysql');
+const mysql = require('mysql');
+require('dotenv').config();
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+app.get('/connect:/value', (req, res) => {
+    res.send('Hello World!')
+    const value= req.params.value
+    con.connect(function (err) {
+        if (err) throw err;
+        con.query(`SELECT * FROM city where name='${value}'`, function (err, result, fields) {
+            if (err) throw err;
+            console.log(result);
+            console.log(result.Name);
+        });
+    });
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
 
 var con = mysql.createConnection({
     host: "localhost",
-    user: "",
-    password: "",
+    user: process.env.USER,
+    password: process.env.PASS,
     database: "world"
 });
 
-
-con.connect(function (err) {
-    if (err) throw err;
-    con.query("SELECT * FROM city where name='chandigarh'", function (err, result, fields) {
-        if (err) throw err;
-
-        console.log(result);
-        console.log(result.Name);
-    });
-});
