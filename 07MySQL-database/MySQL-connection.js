e23c186c0b94e62c14ddf37435b7db0416151e54
Ashish <127130546+akrathor18@users.noreply.github.com>
2024-07-16 14:20:02 +0530

    commit

--- 07MySQL-database/MySQL-connection.js
+++ 07MySQL-database/MySQL-connection.js
@@ -1,12 +1,19 @@
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "ashish"
+    host: "localhost",
+    user: "",
+    password: "",
+    database: "world"
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
+
+con.connect(function (err) {
+    if (err) throw err;
+    con.query("SELECT * FROM city where name='chandigarh'", function (err, result, fields) {
+        if (err) throw err;
+
+        console.log(result);
+        console.log(result.Name);
+    });
 });
