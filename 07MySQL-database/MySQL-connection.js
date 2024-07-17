c274e430788f475be7d1bc0e315df7aa51030e14
Ashish <127130546+akrathor18@users.noreply.github.com>
2024-07-17 11:41:13 +0530

    commit

--- 07MySQL-database/MySQL-connection.js
+++ 07MySQL-database/MySQL-connection.js
@@ -1,34 +1,41 @@
 const mysql = require('mysql');
 require('dotenv').config();
 
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-app.get('/connect:/value', (req, res) => {
-    res.send('Hello World!')
-    const value= req.params.value
-    con.connect(function (err) {
-        if (err) throw err;
-        con.query(`SELECT * FROM city where name='${value}'`, function (err, result, fields) {
-            if (err) throw err;
-            console.log(result);
-            console.log(result.Name);
-        });
-    });
-})
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
-
-var con = mysql.createConnection({
+// Create a connection pool
+const pool = mysql.createPool({
+    connectionLimit: 10, // Adjust this based on your application's needs
     host: "localhost",
     user: process.env.USER,
     password: process.env.PASS,
     database: "world"
 });
 
+app.get('/', (req, res) => {
+    pool.query(`SELECT * FROM city `, (err, results) => {
+        if (err) {
+            console.error('Error executing query:', err);
+            res.status(500).send('Error executing query');
+            return;
+        }
+        res.json(results);
+});
+});
+app.get('/connect/:value', (req, res) => {
+    const value = req.params.value;
+    pool.query(`SELECT * FROM city WHERE name = ?`, [value], (err, results) => {
+        if (err) {
+            console.error('Error executing query:', err);
+            res.status(500).send('Error executing query');
+            return;
+        }
+        res.json(results);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
