2fae37d654b362c652585754ba03309f13771b84
Ashish <127130546+akrathor18@users.noreply.github.com>
2024-07-26 14:03:39 +0530

    commit

--- 09Mongodb-node/index.js
+++ 09Mongodb-node/index.js
@@ -1,1 +1,17 @@
-code 
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function(err, client) {
+  if (err) {
+    console.error("Failed to connect to the MongoDB server. Error:", err);
+    return;
+  }
+  console.log("Connected to the MongoDB server!");
+  
+  var db = client.db("learnDbs");
+  
+  // Database operations can be performed here
+  
+  client.close(); // Close the connection when done
+});
+
