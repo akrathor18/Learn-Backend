9c92db39bcfb269f6dffdab783e3d65c70b33034
Ashish <127130546+akrathor18@users.noreply.github.com>
2024-07-30 19:22:53 +0530

    commit

--- 09Mongodb-node/index.js
+++ 09Mongodb-node/index.js
@@ -1,26 +1,13 @@
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/";
+var url = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000";
 
 console.log("Attempting to connect to MongoDB...");
 
 MongoClient.connect(url, function(err, client) {
   if (err) {
-    console.error("Failed to connect to the MongoDB server. Error:", err);
-    return;
+    console.error("Error:", err);
+  } else {
+    console.log("Connected to the MongoDB server!");
+    client.close();
   }
-  console.log("Connected to the MongoDB server!");
-  
-  var db = client.db("learnDbs");
-  
-  // Example operation
-  db.collection('user').find({}).toArray(function(err, result) {
-    if (err) {
-      console.error("Error fetching data from 'test' collection:", err);
-    } else {
-      console.log("Data from 'test' collection:", result);
-    }
-    
-    client.close(); // Close the connection when done
-  });
 });
-
