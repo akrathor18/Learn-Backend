960a722baa54be124f33dc211946409523bfc99d
Ashish <127130546+akrathor18@users.noreply.github.com>
2024-07-30 20:02:43 +0530

    database connection

---
+++ 09Mongodb-node/database.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const url= 'mongodb://127.0.0.1:27017/learndbs'
+
+mongoose.connect(url,{
+    useNewUrlParser:true,
+})
+
+const db= mongoose.connection;
+db.on('connected',()=>{
+    console.log("Database connected");
+})
+db.on('error',(err)=>{
+    console.log('Failed to connecting with database', err);
+})
+db.on('disconnected',()=>{
+    console.log("Database disconnected");
+})
+
+module.exports= db;
