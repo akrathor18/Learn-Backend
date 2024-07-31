d8078132eecc4843ebacaf6f3ed51750231047ea
Ashish <127130546+akrathor18@users.noreply.github.com>
2024-07-31 19:56:35 +0530

    commit

--- 09Mongodb-node/server.js
+++ 09Mongodb-node/server.js
@@ -17,9 +17,7 @@ app.post('/posts', (req, res) => {
   console.log(newPost);
   res.status(201).json('Data post sucessfully')
 
-});('/', (req, res) => {
-  res.send('Hello World!')
-})
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
