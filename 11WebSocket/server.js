1862d1e66e6ce3a7df606ddcb1e26f655d4ae9db
Ashish <127130546+akrathor18@users.noreply.github.com>
2024-08-27 20:06:52 +0530

    changing ui

--- 11WebSocket/server.js
+++ 11WebSocket/server.js
@@ -3,28 +3,27 @@ const express = require("express");
 const path = require("path");
 const { Server } = require("socket.io");
 const port = 3000;
-
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
 io.on("connection", (socket) => {
-    console.log("A user connected");
-    console.log("A user connected with ID:", socket.id);
-    socket.on("chat message", (message) => {
-      console.log(`Message from ${socket.id}:`, message);
-      io.emit("chat message", `${socket.id}: ${message}`);
+  console.log("A user connected");
+  console.log("A user connected with ID:", socket.id);
+  socket.on("chat message", (message) => {
+    console.log(`Message from ${socket.id}:`, message);
+    io.emit("chat message", `${socket.id}: ${message}`);
   });
 
   socket.on("disconnect", () => {
-      console.log(`User with ID ${socket.id} disconnected`);
+    console.log(`User with ID ${socket.id} disconnected`);
   });
 });
 
 app.use(express.static(path.resolve("./public")));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve("./public/index.html"));
+  res.sendFile(path.resolve("./public/index.html"));
 });
 
 server.listen(port, () => console.log(`Server started at PORT: ${port}`));
