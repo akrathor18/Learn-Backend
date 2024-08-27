6ecf78a39211bc6753ba3ea93f6fbed671281843
Ashish <127130546+akrathor18@users.noreply.github.com>
2024-08-27 19:37:26 +0530

    stablished connection btw server and clint

--- 11WebSocket/server.js
+++ 11WebSocket/server.js
@@ -2,29 +2,29 @@ const http = require("http");
 const express = require("express");
 const path = require("path");
 const { Server } = require("socket.io");
+const port = 3000;
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// Socket.io
-// io.on("connection", (socket) => {
-//   socket.on("user-message", (message) => {
-//     io.emit("message", message);
-//   });
-// });
 io.on("connection", (socket) => {
-    socket.on("user-message", (message) => {
-      console.log("Message received:", message); // Add this line
-      io.emit("message", message);
+    console.log("A user connected");
+
+    socket.on("chat message", (message) => {
+        console.log("Message received:", message);
+        io.emit("chat message", message); // Broadcasts the message to all connected clients
     });
-  });
-  
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected");
+    });
+});
 
 app.use(express.static(path.resolve("./public")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve("./public/index.html"));
+    res.sendFile(path.resolve("./public/index.html"));
 });
 
-server.listen(3000, () => console.log(`Server Started at PORT:3000`));
+server.listen(port, () => console.log(`Server started at PORT: ${port}`));
